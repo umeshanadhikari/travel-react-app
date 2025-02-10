6c79e300f96bb7403cbaad63928af67d553a3b11
AMUC Adhikari <21it0451@itum.mrt.ac.lk>
2025-02-10 23:40:24 +0530

    Added responsive home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import './home.css'
 import video from '../../Assets/video1.mp4'
+import './home.scss'
+import {GrLocation} from 'react-icons/gr'
+import {HiFilter} from 'react-icons/hi'
+import { FiFacebook } from 'react-icons/fi'
+import { AiOutlineInstagram } from 'react-icons/ai'
+import { BsListTask } from 'react-icons/bs'
+import { TbApps } from 'react-icons/tb'
+import { SiTripadvisor } from 'react-icons/si'
+
 
 const Home = () => {
   return (
@@ -10,12 +19,60 @@ const Home = () => {
 
       <div className="homeContent container">
         <div className="textDiv">
+
           <span className="smallText">
             our packages
           </span>
+
           <h1 className="homeTitle">
             Search Your Holiday
           </h1>
+
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search your destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder='Enter name here....'/>
+             <GrLocation className="icon"/>
+            </div>
+          </div>
+
+          <div className="dateInput">
+            <label htmlFor="date">Select your date:</label>
+            <div className="input flex">
+              <input type="date"/>
+            </div>
+          </div>
+
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000"/>
+            </div>
+          </div>
+
+          <div className="searchOptions flex">
+            <HiFilter className="icon"/>
+            <span>MORE FILTERS</span>
+          </div>
+        </div>
+
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className='icon'/>
+            <AiOutlineInstagram className='icon'/>
+            <SiTripadvisor className='icon'/>
+          </div>
+
+          <div className="leftIcons">
+            <BsListTask className='icon'/>
+            <TbApps className='icon'/>
+          </div>
         </div>
       </div>
     </section>
